859e970db0e38dfae454beec6ed8be3bb5c4843c
sdarek <81914998+sdarek@users.noreply.github.com>
2025-04-30 17:06:39 +0200

    WSNC-17 Konfiguracja bibliotek i utworzenie struktur folderów # added basic libraries # edited index file for testing Paper library

--- frontend/cashapp/app/index.tsx
+++ frontend/cashapp/app/index.tsx
@@ -1,4 +1,6 @@
+import * as React from 'react'
 import { Text, View } from "react-native";
+import { PaperProvider, Button } from 'react-native-paper';
 
 export default function Index() {
   return (
@@ -9,7 +11,18 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <PaperProvider>
+        <Text style={{ marginTop: 50, textAlign: 'center'}}>Hello wariaciku</Text>
+        <Button
+          mode="contained"
+          onPress={() => console.log('Wariacie kliknales')}
+          style={{ margin: 20 }}
+        >
+          Kliknij
+        </Button>
+
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </PaperProvider>
     </View>
   );
 }
