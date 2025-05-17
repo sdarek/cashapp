83f62a97828a244583797ed1e73549eb52eac38e
sdarek <81914998+sdarek@users.noreply.github.com>
2025-05-17 02:22:28 +0200

    WSNC-54 Zmienne środowiskowe

--- frontend/cashapp/services/api.ts
+++ frontend/cashapp/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
-const API_BASE_URL = 'http://192.168.1.102:8080/api'; //TU ADRES SWOJEGO KOMPA
+const API_BASE_URL = 'http://192.168.1.102:8080/api'; //http://<TWOJE_IP>:8080/api
 
 export const ping = async () => {
-  const response = await axios.get(`${API_BASE_URL}/ping`);
+  const url = `${process.env.EXPO_PUBLIC_API_BASE_URL}/ping`;
+  const response = await axios.get(url);
   return response.data;
 };
